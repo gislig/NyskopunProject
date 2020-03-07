310aff9c97a4265c724ce9bffc81d9aaa509d142
Gísli Guðmundsson <gisli@iteggs.com>
2020-03-07 22:53:44 +0000

    small demo

---
+++ server.py
@@ -0,0 +1,35 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+device = [
+    {
+        'id': 0,
+        'pid': 1234,
+        'product_name':'rotor1',
+        'product_handbook': 'somebook',
+        'product_items': 'item1, item 2',
+        'product_maintenance_schedule': 'item1 - mondays at 08:00'
+    },
+    {
+        'id': 1,
+        'pid': 4321,
+        'product_name':'rotor2',
+        'product_handbook': 'somebook',
+        'product_items': 'item1, item 2',
+        'product_maintenance_schedule': 'item1 - mondays at 08:00'
+    },       
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    return "This is it"
+
+
+@app.route('/api/v1/resources/products/all', methods=['GET'])
+def api_all():
+    return jsonify(device)
+
+app.run()
